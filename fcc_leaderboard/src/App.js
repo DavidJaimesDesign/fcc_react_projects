3c6d503b1e0b76ac01d10df891633c88635989a4
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-19 16:44:56 -0800

    Figured out how to fix issue with vim tabs set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab in ./vimrc

--- fcc_leaderboard/src/App.js
+++ fcc_leaderboard/src/App.js
@@ -2,27 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const thirtydays = [{
-						"username":"sjames1958gm",
-						"img":"https://avatars.githubusercontent.com/u/4639625?v=3",
-						"alltime":4620,
-						"recent":535,
-						"lastUpdate":"2016-12-19T13:21:24.711Z",
-				},
-				]
+const thirtydays = [{"username":"sjames1958gm","img":"https://avatars.githubusercontent.com/u/4639625?v=3","alltime":4620,"recent":535,"lastUpdate":"2016-12-19T13:21:24.711Z"},{"username":"apottr","img":"https://avatars.githubusercontent.com/u/1924021?v=3","alltime":1113,"recent":464,"lastUpdate":"2016-12-12T21:53:18.499Z"},{"username":"diomed","img":"https://avatars.githubusercontent.com/u/72777?v=3","alltime":1661,"recent":463,"lastUpdate":"2016-12-12T12:06:39.303Z"},{"username":"Blauelf","img":"https://avatars.githubusercontent.com/u/5952026?v=3","alltime":2879,"recent":372,"lastUpdate":"2016-12-12T13:04:09.500Z"},{"username":"anthonygallina1","img":"https://avatars.githubusercontent.com/u/11003055?v=3","alltime":2486,"recent":291,"lastUpdate":"2016-12-12T13:04:08.423Z"},{"username":"revisualize","img":"https://avatars.githubusercontent.com/u/1588399?v=3","alltime":2287,"recent":289,"lastUpdate":"2016-12-12T12:05:00.476Z"},{"username":"Chrono79","img":"https://avatars.githubusercontent.com/u/9571508?v=3","alltime":2467,"recent":273,"lastUpdate":"2016-12-12T12:04:35.292Z"},]
 
-class App extends Component {
+class App extends Component { 
 	render() {
 		return (
 			<div className="App">
 				{ thirtydays.map((user) =>
-						<div key = {user.username}>
-								<span>{user.username}</span>
-								<span>{user.img}</span>
-								<span>{user.alltime}</span>
-								<span>{user.recent}</span>
-								<span>{user.lastUpdate}</span>
-						</div>
+				    <div key = {user.username}>
+					    <span>{user.username}</span>
+						<span>{user.img}</span>
+						<span>{user.alltime}</span>
+						<span>{user.recent}</span>
+						<span>{user.lastUpdate}</span>
+					</div>
 				)}
       		</div>
    		 );
