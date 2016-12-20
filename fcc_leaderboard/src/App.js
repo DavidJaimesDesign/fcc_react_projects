88ea3a99de9e72d3d4b275ab4a8867ee2cfd5136
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-19 16:32:26 -0800

    Basic maping of leaderboard inprogress

--- fcc_leaderboard/src/App.js
+++ fcc_leaderboard/src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const thirtydays = [{
+						"username":"sjames1958gm",
+						"img":"https://avatars.githubusercontent.com/u/4639625?v=3",
+						"alltime":4620,
+						"recent":535,
+						"lastUpdate":"2016-12-19T13:21:24.711Z",
+				},
+				]
 
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				{ thirtydays.map((user) =>
+						<div key = {user.username}>
+								<span>{user.username}</span>
+								<span>{user.img}</span>
+								<span>{user.alltime}</span>
+								<span>{user.recent}</span>
+								<span>{user.lastUpdate}</span>
+						</div>
+				)}
+      		</div>
+   		 );
+  	}
+}
 export default App;
