fdf02bd91809f9799b9caa61829c4e55ac2544b1
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-20 06:28:34 -0800

    Table formated now working on avatar picture

--- fcc_leaderboard/src/App.js
+++ fcc_leaderboard/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Table } from 'react-bootstrap';
 
 const thirtydays = [{"username":"sjames1958gm","img":"https://avatars.githubusercontent.com/u/4639625?v=3","alltime":4620,"recent":535,"lastUpdate":"2016-12-19T13:21:24.711Z"},{"username":"apottr","img":"https://avatars.githubusercontent.com/u/1924021?v=3","alltime":1113,"recent":464,"lastUpdate":"2016-12-12T21:53:18.499Z"},{"username":"diomed","img":"https://avatars.githubusercontent.com/u/72777?v=3","alltime":1661,"recent":463,"lastUpdate":"2016-12-12T12:06:39.303Z"},{"username":"Blauelf","img":"https://avatars.githubusercontent.com/u/5952026?v=3","alltime":2879,"recent":372,"lastUpdate":"2016-12-12T13:04:09.500Z"},{"username":"anthonygallina1","img":"https://avatars.githubusercontent.com/u/11003055?v=3","alltime":2486,"recent":291,"lastUpdate":"2016-12-12T13:04:08.423Z"},{"username":"revisualize","img":"https://avatars.githubusercontent.com/u/1588399?v=3","alltime":2287,"recent":289,"lastUpdate":"2016-12-12T12:05:00.476Z"},{"username":"Chrono79","img":"https://avatars.githubusercontent.com/u/9571508?v=3","alltime":2467,"recent":273,"lastUpdate":"2016-12-12T12:04:35.292Z"},]
 
@@ -20,43 +20,46 @@ class App extends Component {
         return (
             <Grid>
                 <div className="App">
-                    <div class="container">
-                        <div bclass="row">
+                        <Row>
                             <h1>Free Code Camp Leaderboard</h1>
-                        </div>
-                        <div bclass="row">
-                                <Col md= {12}/>
-      	                        <Table thirtydays={thirtydays}/>
-                        </div>
-                    </div>
+                        </Row>
+                        <Row>
+                            <Col md={12}>
+      	                        <LeaderBoard thirtydays={thirtydays}/>
+                            </Col>
+                        </Row>
                 </div>
             </Grid>
    	);
     }
 }
 
-class Table extends Component {
+class LeaderBoard extends Component {
     render (){
         const { thirtydays } = this.props
         return(
-            <table>
-                <tr>
-                    <th>Username</th>
-                    <th>Image</th>
-                    <th>Alltime</th>
-                    <th>30 days</th>
-                    <th>Last update</th>
-                </tr>
-	    	{thirtydays.map((user) =>
-		<tr>
-		    <td>{user.username}</td>
-		    <td>{user.img}</td>
-		    <td>{user.alltime}</td>
-		    <td>{user.recent}</td>
-		    <td>{user.lastUpdate}</td>
-		</tr>
-	        )}
-            </table>
+            <Table striped bordered condensed hover>
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Image</th>
+                        <th>Alltime</th>
+                        <th>30 days</th>
+                        <th>Last update</th>
+                    </tr>
+                </thead>
+                <tbody>
+	    	    {thirtydays.map((user) =>
+		    <tr>
+		        <td>{user.username}</td>
+		        <td>{user.img}</td>
+		        <td>{user.alltime}</td>
+		        <td>{user.recent}</td>
+		        <td>{user.lastUpdate}</td>
+		    </tr>
+	            )}
+                </tbody>
+            </Table>
         )
     }
 }
