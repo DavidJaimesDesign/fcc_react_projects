c4c25c941f90635c789170043aa7f897bbb88bdf
David Jaimes <davidjaimesdesign@gmail.com>
2017-01-10 15:23:40 -0800

    Added the random array generator

--- fcc_game_of_life/src/App.js
+++ fcc_game_of_life/src/App.js
@@ -1,3 +1,4 @@
+//the 50 by 70 grid is static
 import React, { Component } from 'react';
 import './App.css';
 import GenerationCount from './components/generationcount'
@@ -6,9 +7,29 @@ class App extends Component {
         super(props);
 
         this.state = {
-            generationCount: 0
+            generationCount: 0,
+            cellArray: null
         }
     }
+
+    generateRandomCellArray(){
+        let cellArray = []
+        for(var i=0; i<50; i++ ){
+            let row = [];
+            for(var j=0; j<70; j++){
+                var cellvalue = Math.random() < 0.5 ? true : false;
+                row.push(cellvalue)
+            }
+            cellArray.push(row)   
+        }
+        return cellArray
+    }
+
+    componentWillMount(){
+        let arrayTest = this.generateRandomCellArray();
+        console.log(arrayTest)
+    }
+
     render() {
         return (
             <div>
