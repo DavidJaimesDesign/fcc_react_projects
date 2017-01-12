ecd1e6acff2b7736ecf013fac26c994433d07b8f
David Jaimes <davidjaimesdesign@gmail.com>
2017-01-12 05:51:50 -0800

    map the cells randomly works now on to the algo of life and death

--- fcc_game_of_life/src/components/displaycells.js
+++ fcc_game_of_life/src/components/displaycells.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const DisplayCells = ({array}) => {
+    function checkCell(cell, y){
+        let cellVal;
+        if(cell === true){
+            cellVal = <div className="empty-cell col-centered" key={y}></div>
+        }else {
+            cellVal = <div className="empty-cell filled-cell col-centered" key={y}></div>
+        }
+        return cellVal;
+    }
+    
     return(
             <div className="col-md-8 col-md-offset-2  col-centered displaycells">
                 {array.map((row, k) =>
                     <div className="row" key={k}>
                         {row.map((cell, y) =>
-                            <div className="empty-cell col-centered" key={y}></div>
+                            checkCell(cell, y)
                         )}
                     </div>
                 )}
