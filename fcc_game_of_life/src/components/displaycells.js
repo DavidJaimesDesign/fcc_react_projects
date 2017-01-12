4f5a20775691a47ce797e5041215f39431a50eca
David Jaimes <davidjaimesdesign@gmail.com>
2017-01-11 16:00:24 -0800

    basic layout of the grid works could still use some tweaking

--- fcc_game_of_life/src/components/displaycells.js
+++ fcc_game_of_life/src/components/displaycells.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const DisplayCells = ({array}) => {
     return(
-            <div className="col-md-12  col-centered displaycells">
+            <div className="col-md-8 col-md-offset-2  col-centered displaycells">
                 {array.map((row, k) =>
                     <div className="row" key={k}>
                         {row.map((cell, y) =>
-                            <div className="empty-cell" key={y}></div>
+                            <div className="empty-cell col-centered" key={y}></div>
                         )}
                     </div>
                 )}
