02ad4af06cc0b1ee02cc20c4b0efb19229bc2faf
David Jaimes <davidjaimesdesign@gmail.com>
2017-01-16 14:29:12 -0800

    Ok so it is most definitely not state but a problem with the array being fine in the debugger but when called it is blank

--- fcc_game_of_life/src/App.js
+++ fcc_game_of_life/src/App.js
@@ -34,23 +34,22 @@ class App extends Component {
     }
 
     cellLives(xCoord, yCoord){
-        debugger;
         let count = 0;
         const y = this.state.yaxis
         const x = this.state.xaxis
-        
+        let arrayCheck = this.state.cellArray
         for(var i=-1;i<=1;i++){
             for(var j=-1;j<=1;j++){
                 if((xCoord + i) >= 0 && (xCoord + 1) < x && (yCoord + j) >= 0 && (yCoord + j) < y && !(i === 0 && j === 0)){
                    //I am getting undefined here after one cycle I know this now might do with the this.state 
-                    if(this.state.cellArray[xCoord+i][yCoord+j] === true){
+                    if(arrayCheck[xCoord+i][yCoord+j] === true){
                         count++;
                     }
                 }
             }
         }
        //console.log(count)
-        if(count === 3 || (this.state.cellArray[xCoord][yCoord] && count === 2)){
+        if(count === 3 || (arrayCheck[xCoord][yCoord] && count === 2)){
             count = 0
             return true
         } else {
@@ -62,12 +61,10 @@ class App extends Component {
        
     newGeneration(){
         let newGenerationArray = [];
-        const x = this.state.xaxis
-        const y = this.state.yaxis
 
-        for(var i=0; i<y; i++){
+        for(var i=0; i<50; i++){
            newGenerationArray.push([])
-            for(var j=0; j<x; j++){
+            for(var j=0; j<70; j++){
                 newGenerationArray[i].push(this.cellLives(i, j))
             }  
         }
@@ -80,6 +77,7 @@ class App extends Component {
     }
 
     componentWillMount(){
+        debugger;
         let cellArray = this.generateRandomCellArray()
         this.setState({cellArray})
         //console.log(this.state.cellArray[0][1])
